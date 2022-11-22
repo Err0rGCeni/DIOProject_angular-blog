d3a63186148950862d683525e00833247e0b8e9e
gceni <gceni10@gmail.com>
2022-11-22 16:29:16 -0300

    finished

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "0",
+    "title": "Estranho evento ocorre em Night City!",
+    "description": "Fenômeno intriga meteorologistas e modders!",
+    "photoCover": "../assets/greenthundernightcity.jpg"
+  },
+  {
+    "id": "1",
+    "title": "Novas novidades da nova missão do novo shihtzunauta!",
+    "description": "Em entrevista exclusiva, disse o nosso herói: 'Au au, au au!'",
+    "photoCover": "../assets/shihtzunaut.jpg"
+  },
+  {
+    "id": "2",
+    "title": "Hora de plantar uma árvore: 5 Motivos.",
+    "description": "Primeiro motivo: Por que não? Outros 4 motivos: Primeiro motivo.",
+    "photoCover": "../assets/treeclock.jpg"
+  },
+  {
+    "id": "3",
+    "title": "Tartarugas douradas não estão em extinção.",
+    "description": "O mistério chega ao fim! As tartarugas douradas não estão em extinção, uma vez que não existem.",
+    "photoCover": "../assets/goldenturtle.jpg"
+  },
+]
